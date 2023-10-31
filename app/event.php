bc2af5c860e285e8e2e7f0eccce8e2c10173825a
한대승 <hoksi3k@gmail.com>
2023-10-31 19:32:04 +0900

    event 추가

--- app/event.php
+++ app/event.php
@@ -1,14 +1,56 @@
 <?php
-Swoole\Coroutine\run(function() {
-    $fp = stream_socket_client("tcp://www.qq.com:80", $errno, $errstr, 30);
-    fwrite($fp,"GET / HTTP/1.1\r\nHost: www.qq.com\r\n\r\n");
+use Swoole\Coroutine;
+use Swoole\Database\PDOConfig;
+use Swoole\Database\PDOPool;
+use Swoole\Runtime;
 
-    Swoole\Event::add($fp, function($fp) {
-        $resp = fread($fp, 8192);
-        Swoole\Event::del($fp);
-        fclose($fp);
-        print_r($resp);
+// APP_PATH 상수를 정의합니다.
+if (strchr(PHP_OS, 'CYGWIN')) {
+    defined('APP_PATH') or define('APP_PATH', __DIR__ . '/app');
+} else {
+    defined('APP_PATH') or define('APP_PATH', __DIR__);
+}
+// public 디렉터리의 경로를 정의합니다.
+defined('PUBLIC_PATH') or define('PUBLIC_PATH', realpath(APP_PATH . '/../public'));
+defined('THIRDPARTY_PATH') or define('THIRDPARTY_PATH', realpath(APP_PATH . '/../third-party'));
+
+require_once THIRDPARTY_PATH . '/vendor/autoload.php';
+
+$events = new \Laminas\EventManager\EventManager();
+$events->attach('test', function ($e) {
+    $event = $e->getName();
+    $params = $e->getParams();
+    printf(
+        'Handled event "%s", with parameters %s' . PHP_EOL,
+        $event,
+        json_encode($params)
+    );
+
+    return ['params' => $params, 'result' => 'ok'];
+});
+
+Coroutine\run(function () use ($events) {
+    $pool = new PDOPool((new PDOConfig)
+        ->withHost('host.docker.internal')
+        ->withPort(3306)
+        ->withDbName('swoole')
+        ->withCharset('utf8mb4')
+        ->withUsername('swoole')
+        ->withPassword('swoole')
+    );
+
+    var_dump($pool->get());
+
+    go(function () use ($events) {
+        sleep(1);
+        $res = $events->trigger('test', null, ['1', 'bar', microtime(true)]);
+    });
+
+    go(function () use ($events) {
+        $res = $events->trigger('test', null, ['2', 'bar', microtime(true)]);
+    });
+
+    go(function () use ($events) {
+        $res = $events->trigger('test', null, ['3', 'bar', microtime(true)]);
     });
 });
-echo "Finish\n";
-
